26ea62da89b4816abcd11308289555dc38ed81ed
kevinelstri <1098918523@qq.com>
2017-08-07 22:04:40 +0800

    stack

---
+++ Problem Solving with Algorithms and Data Structures using python/Stack/Reverse_string.py
@@ -0,0 +1,18 @@
+﻿# coding:utf-8
+
+from stack_list_order import Stack
+
+
+# implement the reverse string
+def Reverse(r_string):
+    s = Stack()
+    Reverse_string = ''
+    for r in r_string:
+        s.push(r)
+    while not s.is_empty():
+        Reverse_string += s.pop()
+    return Reverse_string
+
+
+r_string = '123hwehfdweif'
+print Reverse(r_string)
